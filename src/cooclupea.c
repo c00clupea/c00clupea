c2787b52d746e21caf0b852e1b935800c683d7d6
dev <develop@develop.my.domain>
2014-07-04 13:11:33 +0200

    nightly build -working

--- src/cooclupea.c
+++ src/cooclupea.c
@@ -21,16 +21,17 @@ static ringbuffer_t 	*buf_main_consumer_command;
 static int 		(*strategy_ptr[STRAT_LEN])(struct consumer_command *) 	= {NULL};
 
 int(*read_strategy_from_idx(int idx))(struct consumer_command *){
-  if(idx < 0 && idx > (STRAT_LEN -1)){
-    syslog(LOG_ERR,"You tried strategy %d which is out of bound",idx);
-    return strategy_ptr[STRAT_DEFAULT];
-  }
-  return strategy_ptr[idx];
+	if(idx < 0 && idx > (STRAT_LEN -1)){
+    		syslog(LOG_ERR,"You tried strategy %d which is out of bound",idx);
+    		return strategy_ptr[STRAT_DEFAULT];
+  	}
+  	return strategy_ptr[idx];
 }
 
 
-int init_strategies(){
-  strategy_ptr[0] = &strategy_dev_null;
+int init_strategies()
+{
+        strategy_ptr[0] = &strategy_dev_null;
 	return 0;
 }
 
