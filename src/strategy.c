5b018d39d471c59c65933ca1219c474e17f77a62
dev <develop@develop.my.domain>
2014-07-23 17:39:53 +0200

    nightly build -working

--- src/strategy.c
+++ src/strategy.c
@@ -7,13 +7,13 @@ int strategy_dev_null(struct consumer_command *tmp_cmd){
 	const char* result=inet_ntop(AF_INET,&(tmp_cmd->client.sin_addr),buffer,sizeof(buffer));
 	//syslog(STDLOG,"Yeehaa i got some date from %s",result);  
 	while(1){
-		int len = 0;
+		int len = 1024;
 	  	char *input;
 	  	if (read (tmp_cmd->peer_socket, &len, sizeof (len)) == 0){
       			break;
 	  	}	
 	  	input = (char*)malloc(len);
-	  	read(tmp_cmd->peer_socket,input,len);
+	  	read(tmp_cmd->peer_socket,input,len-1);
 	  	syslog(STDLOG,"rcv len %d bytes input:%s\n",len,input);
 	  	free(input);
 	}
