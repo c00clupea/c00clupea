a382b1236cce5c51ba9c99bbe2b0e7eadd7dad8e
dev <develop@develop.my.domain>
2014-07-24 17:59:34 +0200

    nightly build -working

--- src/strategy.h
+++ src/strategy.h
@@ -5,9 +5,12 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include "cooclupea.h"
+
+/**Strategies**/
+
 #include "strategy_dev_null.h"
+#include "strategy_http_simple.h"
 
 
-int strategy_http_simple(struct consumer_command *tmp_cmd);
 
 #endif
---
+++ src/strategy_http_simple.h
@@ -0,0 +1,7 @@
+#ifndef STRATEG_HTTP_SIMPLE_H
+#define STRATEG_HTTP_SIMPLE_H
+#include "strategy.h"
+
+
+int strategy_http_simple(struct consumer_command *tmp_cmd);
+#endif
