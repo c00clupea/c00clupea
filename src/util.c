9358bee6d053690bd20d0a00af0f8f4af4d4ea1a
dev <develop@develop.my.domain>
2014-07-24 20:19:45 +0200

    nightly build -working

--- src/util.c
+++ src/util.c
@@ -10,3 +10,17 @@ int print_log(FILE *fp, char *text){
 	return res;
 	
 }	
+
+int print_safe_log(pthread_mutex_t *mtx, FILE *fp, char *txt){
+	if(pthread_mutex_lock(mtx) != 0){
+		syslog(LOG_ERR,"logwriter is not able to lock");
+		return 1;
+	}
+
+	fprintf(fp,txt);
+	if(pthread_mutex_unlock(mtx)!=0){
+		syslog(LOG_ERR,"problem with unlock in consumer");
+		return 1;
+	}
+
+}
--- src/util.h
+++ src/util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 #include <stdio.h>
 #include <pthread.h>
+#include <syslog.h>
 
 int print_log(FILE *fp, char *text);
 
