1bff5acecf419ac7040b9bdd77fc7fa49c4247d3
dev <develop@develop.my.domain>
2014-07-24 18:04:02 +0200

    nightly build -working

--- src/cooclupea.c
+++ src/cooclupea.c
@@ -17,25 +17,6 @@ pthread_cond_t  	buf_main_consumer_not_empty_cond			= PTHREAD_COND_INITIALIZER;
 static char 		*main_config;
 
 static ringbuffer_t 	*buf_main_consumer_command;
-/**
-static int 		(*strategy_ptr[STRAT_LEN])(struct consumer_command *) 	= {NULL};
-
-int(*read_strategy_from_idx(int idx))(struct consumer_command *){
-	if(idx < 0 && idx > (STRAT_LEN -1)){
-    		syslog(LOG_ERR,"You tried strategy %d which is out of bound",idx);
-    		return strategy_ptr[STRAT_DEFAULT];
-  	}
-  	return strategy_ptr[idx];
-}
-
-
-int init_strategies()
-{
-        strategy_ptr[0] = &strategy_dev_null;
-	strategy_ptr[1] = &strategy_http_simple;
-	return 0;
-}
-**/
 
 int destroy_consumer_command(struct consumer_command *tmp_cmd){
   //Never ever free serverConfig here....you will need it
