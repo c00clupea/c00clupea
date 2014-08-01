5ff6b5fee11eae8f353210c014ac6ef57f55d275
c00clupea <c00clupea@gmail.com>
2014-08-01 12:43:41 +0200

    +utils

--- src/util.c
+++ src/util.c
@@ -1,10 +1,19 @@
 #include "util.h"
+
+int c00_seek_file(FILE *fp,int offset);
+
+int c00_seek_file(FILE *fp,int offset){
+	if(fseek(fp,offset,SEEK_SET) == -1){
+		return 1;
+	}
+	return 0;
+}
 	
 struct safe_log *init_safe_log( char *file_name){
 	struct safe_log *logger = malloc(sizeof(struct safe_log));
 	logger->mtx = malloc(sizeof(pthread_mutex_t));
 	pthread_mutex_init(logger->mtx,NULL);
-	logger->fp = fopen(file_name,"w");
+	logger->fp = fopen(file_name,"a");
 	logger->flush_count = 0;
 	if(!logger->fp){
 		syslog(LOG_ERR,"Sorry but %s is not writable or permission problem or sth else",file_name);
@@ -31,6 +40,8 @@ int change_safe_log_file(struct safe_log *logger, char *filename){
 	return 0;
 }
 
+
+
 int close_safe_log(struct safe_log *logger){
 	fclose(logger->fp);
 	free(logger->file_name);
@@ -45,6 +56,127 @@ void flush_log(struct safe_log *logger){
 		fflush(logger->fp);
 	}
 	logger->flush_count = 0;
+}
+
+int print_safe_single_log(struct safe_log *logger, int log_level, char *file, char *txt,...){
+	
+	FILE *fp;
+	fp = fopen(file,"w");
+
+	if(fp){
+
+		fclose(fp);
+	}
+	else{
+		syslog(LOG_ERR,"not able to open %s to write",file);
+		return 1;
+	}
+
+	return 0;
+}
+
+int c00_get_current_time(char *t_buf, char *fmt,int len_buf){
+	time_t now = 0;
+	struct tm *t;
+       	now = time(NULL);
+       	t = gmtime(&now);
+
+       	strftime(t_buf,len_buf - 1 ,fmt,t);
+	return 0;
+}
+
+
+int print_safe_single_log_fr(struct safe_log *logger, int log_lvl,char *file, char *txt, FILE *fr,...){
+	FILE *fp;
+	fp = fopen(file,"w");
+	int chr;
+
+	if(fp){
+
+		char t_buf[31];
+
+		c00_get_current_time(t_buf,"[%Y.%m.%d %T]",31);
+	        
+		fprintf(fp,"[%s]",t_buf);
+
+		va_list args;
+		va_start(args,fr);
+		fprintf(fp,txt,args);
+		va_end(args);
+		fprintf(fp,"\n[PAYLOAD]\n");
+
+		if(c00_seek_file(fr,0) != 0){
+			syslog(LOG_ERR,"CRitical, can not set lseek in fr");
+			fclose(fp);
+			return 1;
+		}
+
+		unsigned long count_len = 0;
+		while((chr = getc(fr)) != EOF || count_len > MAX_FR_TO_LOG_LEN){
+			fprintf(fp,"%c",chr);
+			count_len ++;
+		}
+
+		if(c00_seek_file(fr,0) != 0){
+			syslog(LOG_ERR,"CRitical, can not set lseek in fr");
+			fclose(fp);
+			return 1;
+		}
+
+		fclose(fp);
+	}
+	else{
+		syslog(LOG_ERR,"not able to open %s to write",file);
+		return 1;
+	}
+
+	return 0;
+}
+
+int c00_get_hash_str(char *val,int len){
+   	//This is just the standard ELF-Hash as proposed in many books, blogs, and so on
+	unsigned int hash = 0;
+   	unsigned int x    = 0;
+   	unsigned int i    = 0;
+	char *str      	  = val;
+	
+   	for(i = 0; i < len; str++, i++)
+   	{
+      		hash = (hash << 4) + (*str);
+      		if((x = hash & 0xF0000000L) != 0)
+      		{
+         		hash ^= (x >> 24);
+      		}
+      		hash &= ~x;
+   	}
+
+   return hash;
+
+
+}	
+
+int c00_get_hash_int(int val){
+	return val % HASH_LEN;	
+}
+
+int c00_create_unique_log_file(char *append,char *end, char *filename){
+	
+	char time[31];
+	int hash = 0;
+	char to_hash[PATH_MAX];
+	char simple_file[PATH_MAX];
+
+	c00_get_current_time(time,"[%Y.%m.%d %T]",31);
+
+	snprintf(to_hash,PATH_MAX,"%s_%s.%s",time,append,end);
+
+	hash = c00_get_hash_str(to_hash,PATH_MAX);
+
+	snprintf(simple_file,PATH_MAX,"%s_%d_%s.%s",time,hash,append,end);
+	
+	snprintf(filename,PATH_MAX,LOG_MAIN_PATH,simple_file);
+
+	return 0;
 }
 
 int print_safe_log(struct safe_log *logger, int log_level, char *txt,...){
--- src/util.h
+++ src/util.h
@@ -5,6 +5,8 @@
 #include <syslog.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include "global.h"
 
 #ifndef STD_LOG_LEN 
 #define STD_LOG_LEN 1024
@@ -26,6 +28,9 @@
 #define LOG_LVL_WARN 3
 #define LOG_LVL_ERR 4
 
+#define MAX_FR_TO_LOG_LEN 10485760
+
+
 #define LOGGER(logger,loglvl,fmt,...)\
 	print_safe_log(logger,loglvl,fmt,__VA_ARGS__)
 
@@ -41,6 +46,11 @@
 #define LOGGER_ERROR(logger,fmt,...)\
 	print_safe_log(logger,LOG_LVL_ERR,fmt,__VA_ARGS__)
 
+#define LOGGER_NLOG(logger,loglvl,fp,fmt,...)\
+	print_safe_single_log(logger,loglvl,fp,fmt,__VA_ARGS__)
+
+#define LOGGER_NLOG_FR(logger,loglvl,fmt,fr,...)\
+	print_safe_single_log_fr(logger,loglvl,fmt,fr,__VA_ARGS__)
 
 struct safe_log{
 	char *file_name;
@@ -57,6 +67,10 @@ struct safe_log *init_safe_log(char *file_name);
 int change_safe_log_file(struct safe_log *logger, char *filename);
 int close_safe_log(struct safe_log *logger);
 int print_safe_log(struct safe_log *logger,int log_level, char *txt,...);
-
+int print_safe_single_log_fr(struct safe_log *logger, int log_lvl, char *file, char *txt,FILE *fr,...);
+int print_safe_single_log(struct safe_log *logger, int log_lvl,char *file, char *txt,...);
+int c00_create_unique_log_file(char *append,char *end, char *filename);
+int c00_get_current_time(char *t, char *fmt,int len_buf);
+int c00_get_hash_str(char *val,int len);
 
 #endif
