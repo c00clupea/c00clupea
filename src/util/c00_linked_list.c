7cbf002bdeec667a2e463027a2d4ed30f2e034ff
c00clupea <c00clupea@gmail.com>
2014-08-22 06:47:15 +0200

    +c file

---
+++ src/util/c00_linked_list.c
@@ -0,0 +1,47 @@
+#include "c00_linked_list.h"
+
+
+int c00_linked_list_init(struct c00_linked_list *ptr){
+	return TRUE;
+}
+int c00_linked_list_destroy(struct c00_linked_list *ptr){
+	return TRUE;
+}
+int c00_linked_list_destroy_free(struct c00_linked_list *ptr){
+	return TRUE;
+}
+int c00_linked_list_push_end(struct c00_linked_list *ptr, void *val){
+	return TRUE;
+}
+int c00_linked_list_push_start(struct c00_linked_list *ptr, void *val){
+	return TRUE;
+}
+int c00_linked_list_pop_end(struct c00_linked_list *ptr, void **val){
+	return TRUE;
+}
+int c00_linked_list_pop_start(struct c00_linked_list *ptr, void **val){
+	return TRUE;
+}
+int c00_linked_list_set(struct c00_linked_list *ptr,int idx, void *val){
+	return TRUE;
+}
+int c00_linked_list_get(struct c00_linked_list *ptr,int idx, void **val){
+	return TRUE;
+}
+int c00_linked_list_remove(struct c00_linked_list *ptr, int idx){
+	return TRUE;
+}
+int c00_linked_list_imove(struct c00_linked_list *ptr, signed int range){
+	return TRUE;
+}
+/**i... means index on the current index (move with imove) this action will be done**/
+int c00_linked_list_iget(struct c00_linked_list *ptr, void **val){
+	return TRUE;
+}
+int c00_linked_list_iset(struct c00_linked_list *ptr, void *val){
+	return TRUE;
+}
+int c00_linked_list_iremove(struct c00_linked_list *ptr){
+	return TRUE;
+}
+
