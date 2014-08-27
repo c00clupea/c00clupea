41fe315cb5b4977dba0730be728948e4f40ba91d
c00clupea <c00clupea@gmail.com>
2014-08-27 21:20:56 +0200

    +sim

--- src/c00clupea_sim.c
+++ src/c00clupea_sim.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "c00clupea_sim.h"
 
 int main() {
 	printf("Hello World\n");
---
+++ src/c00clupea_sim.h
@@ -0,0 +1,6 @@
+#ifndef C00CLUPEA_SIM_H
+#define C00CLUPEA_SIM_H
+#include <stdio.h>
+#include "global.h"
+
+#endif
