03cfd2513491ec677d2b52a5ab9c8c2740b93160
c00clupea <c00clupea@gmail.com>
2014-12-17 15:43:37 +0100

    +perftest

--- src/c00clupeaperf.h
+++ src/c00clupeaperf.h
@@ -16,7 +16,59 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>		/* for CHAR_BIT */
 #include "global.h"
+
+#if OSDETECTED == DARWIN
+#include "util/macosx_clock.h" 
+#endif
+
+#define BITMASK(b) (1 << ((b) % CHAR_BIT))
+#define BITSLOT(b) ((b) / CHAR_BIT)
+#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
+#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
+#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+
+#define BILLION  1000000000L;
+
+
+
+#define MAX_BITSET 3
+
+#define MEASURE_TIME 1
+#define MEASURE_MEM 2
+#define MEASURE_VERBOSE 3
+
+#define C00WRITEVERBOSE(fmt,...)		\
+	if(BITTEST(config->flags, MEASURE_VERBOSE)){	\
+		fprintf(stdout,fmt,__VA_ARGS__);	\
+	}		
+
+#define C00WRITEVERBOSEN(fmt)		\
+	if(BITTEST(config->flags, MEASURE_VERBOSE)){	\
+		fprintf(stdout,fmt);	\
+	}
+
+#define C00WRITEN(fmt)		\
+		fprintf(stdout,fmt)	\
+
+
+#define C00WRITE(fmt,...)					\
+	fprintf(stdout,fmt,__VA_ARGS__)					\
+	
+
+
+struct c00_measure_conf{
+	char flags[BITNSLOTS(MAX_BITSET)];
+	char cmd[1024];
+};
+
+struct c00_measure_result{
+	struct timespec *exvptime;
+};
+
+int measure_exvp(struct c00_measure_conf *config,struct c00_measure_result *result);
 
 int main( int argc, char **argv );
 
--- src/global.h
+++ src/global.h
@@ -192,12 +192,18 @@ C00DEBUG("found %s",V);\
 
 
 
+#define check_argv(M,N,O)		\
+	(strcmp(argv[M],N) == 0) || (strcmp(argv[M],O) == 0)	\
 	
 	
 
 
 //Some ideas from http://c.learncodethehardway.org/book/ex20.html
 #define check(A, M, ...) if(!(A)) { C00DEBUG(M, __VA_ARGS__); syslog(LOG_ERR,M,__VA_ARGS__);goto error; }
+
+#define echocheck(A,M,...)if(!(A)) { C00DEBUG(M, __VA_ARGS__); fprintf(stderr,M,__VA_ARGS__);goto error; }
+
+#define echoerror(M,...) C00DEBUG(M, __VA_ARGS__); fprintf(stderr,M,__VA_ARGS__);goto error; 
 
 #define mem_check(A) check((A), "%d Out of memory.",-1)
 
