a9d8fbb117f4ab4a1831cc1770904561bbaf2ee9
c00clupea <c00clupea@gmail.com>
2015-02-10 15:27:48 +0100

    init kernel

---
+++ kernel/src/guardian/guardian.c
@@ -0,0 +1,20 @@
+/**
+ *
+ * cooclupea Honeypot 
+ * <*))><
+ *
+ * (C) 2014 by Christoph Pohl (c00clupea@googlemail.com)
+ * released under the GPLv.2
+ * 
+ * File:	apatebash.c
+ * created: 	Sat Jan 31 16:00:16 2015
+ * author:  	Christoph Pohl <c00clupea@gmail.com>
+ */
+#include "guardian.h"
+
+int main(int argc, char *argv[]){
+	  int i;
+	  for (i=0; i<26; i++) sym[i] = 0;
+	  yyparse();
+	  return 0;
+}
---
+++ kernel/src/guardian/guardian.h
@@ -0,0 +1,25 @@
+/**
+ *
+ * cooclupea Honeypot 
+ * <*))><
+ *
+ * (C) 2014 by Christoph Pohl (c00clupea@googlemail.com)
+ * released under the GPLv.2
+ * 
+ * File:	apatebash.h
+ * created: 	Sat Jan 31 15:59:37 2015
+ * author:  	Christoph Pohl <c00clupea@gmail.com>
+ */
+#ifndef APATEBASH_H
+#define APATEBASH_H
+
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+
+int yyparse(void);
+extern int sym[26];
+
+int main(int argc, char *argv[]);
+
+#endif /* APATEBASH_H */
